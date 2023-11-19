0e8ff2287ddb6971df895d70bd59fcd8b82a00d5
Calvin Josenhans <60663079+HalflingHelper@users.noreply.github.com>
2023-11-18 20:07:33 -0500

    Searchgin

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const path = require('path')
+const books = require('google-books-search'); // https://www.npmjs.com/package/google-books-search
 
-const books = require('google-books-search');
-
-const app = express();
-app.use(express.text({limit:'1mb'}))
 const port = 3000;
 
+const app = express();
+
+app.use(express.text({limit:'1mb'}))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }));
 app.use('/public', express.static('public'));
@@ -16,20 +15,19 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-
-    books.search(req.body, function(error, results) {
+    let list = books.search(req.body, (error, results) => {
         if ( ! error ) {
-            console.log(results);
+            res.json(results);
         } else {
             console.log(error);
         }
     });
-
-    res.json(req.body)
 });
 
+app.use(function(req, res, next){
+    // res.status(404).send("Sorry, page not found");
+    res.status(404).render('404_error.ejs', {title: "Sorry, page not found"});
+
+});
 
 app.listen(port);
-
-// Google Books API package!
-// https://www.npmjs.com/package/google-books-search
